1efbcc9a1e779779820e54a4d44b87ec407a90a3
Basel Bashir <eng.baselbashir@gmail.com>
2023-09-15 19:36:35 +0300

    first commit

---
+++ src/components/WelcomeMessage.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="welcome-message">
+    <h2>Welcome to VueJs</h2>
+    <p>Any</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "WelcomeMessage",
+};
+</script>
+
+<style scoped lang="scss">
+$fontSize: 50px;
+
+h2 {
+  font-size: $fontSize;
+}
+</style>
---
+++ src/views/HelpView.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="help">
+    <h2>Help Page</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelpView",
+};
+</script>
